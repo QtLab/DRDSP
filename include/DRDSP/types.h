81a9f327ccd89d7a013c67bcf670a4fd8cd1349a
cwzx <chriswelshman@gmail.com>
2014-07-09 06:26:15 +0100

    asd

--- include/DRDSP/types.h
+++ include/DRDSP/types.h
@@ -4,8 +4,11 @@
 #include <cstdint>
 
 #pragma warning( disable : 4714 ) // function '...' marked as __forceinline not inlined
+#pragma warning( disable : 4800 ) // 'int' : forcing value to bool 'true' or 'false' (performance warning)
 
 #include <Eigen/Core>
+
+#pragma warning( default : 4800 )
 
 namespace Eigen {
 	typedef Matrix<double,1,1> Vector1d;
